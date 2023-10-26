24c14c5c9dab326be816329ed21a84985a64b359
Bhavani9347 <144900026+Bhavani9347@users.noreply.github.com>
2023-10-26 22:40:23 +0530

    3

---
+++ InventoryDb/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace invenatorydb1ass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             //Dataquery.enterproductinfo(5,"iphonesx",5000,5);
+            // Dataquery.retreiveproducts();
+            //Dataquery.updateproductqty();
+            //Dataquery.entersalesinfo(2, 22, 27, 4500);
+            Dataquery.retreivesales();
+        }
+    }
+}
